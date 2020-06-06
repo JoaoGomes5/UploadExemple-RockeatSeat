91aa7570a1f1fd32aa7883f7887c6741beaf38e7
Joao Gomes <joaopfg.2002@gmail.com>
2020-06-06 20:03:36 +0100

    local save files backend

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'))
+app.use(require('./routes'));
+
+
+
+
+
+
+
+
+app.listen(3333 , () => {
+  console.log('Server running');
+});
